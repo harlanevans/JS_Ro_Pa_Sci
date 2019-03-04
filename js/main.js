bfede7eb21a5617038a93038e8479931ed16ef70
Harlan Evans <harlanevans5@gmail.com>
2019-03-04 15:14:11 -0700

    finished js for game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var userChoice;
-var choices = ["Rock", "Paper", "Scissors"];
+var choice = ["Rock", "Paper", "Scissors"];
 var satanChoice;
-var judgement;
+var result;
 
 var rock = document.body.querySelector(".rock");
 rock.addEventListener("click", function(){
@@ -9,6 +9,20 @@ rock.addEventListener("click", function(){
   alert("You Chose Rock");
   satanChoice();
 })
+
+var paper = document.body.querySelector(".paper");
+paper.addEventListener("click", function () {
+  userChoice = "Paper";
+  alert("You picked Paper!");
+  satanChoice();
+});
+
+var scissors = document.body.querySelector(".scissors");
+scissors.addEventListener("click", function () {
+  userChoice = "Scissors";
+  alert("You picked Scissors!");
+  satanChoice();
+});
 
 function satanChoice(){
   satanResult = choice[Math.floor(Math.random()*choice.length)];
@@ -19,41 +33,38 @@ function compare(){
   switch (satanResult) {
     case "Rock":
     if (userChoice === satanResult) {
-      result = "Draw. Nice. One more chance to dance with the devil"
-    } else if (userChoice === 'scissors') {
-      result = "You lose. You are going to Hell. "
+      result = "Draw. Nice. One more chance to dance with the devil."
+    } else if (userChoice === 'Scissors') {
+      result = "You lose. You are going to Hell."
     } else {
-      result = "Cogratulations, you do not have to go to hell"
+      result = "Congratulations, you do not have to go to hell."
     }
-  }
-  switch (computerResult) {
-    case "Rock":
-      if (userChoice === computerResult) {
-        result = "Draw Idiot"
-      } else if (userChoice === "scissors") {
-        result = "You suck"
-      } else {
-        result = "WINNER WINNER CHICKEN DINNER"
-      }
-      break;
+    break;
     case "Paper":
-      if (userChoice === computerResult) {
-        result = "Draw Idiot"
-      } else if (userChoice === "scissors") {
-        result = "WINNER WINNER CHICKEN DINNER"
+    if (userChoice === satanResult) {
+      result = "Draw. Nice. One more chance to dance with the devil."
+    } else if (userChoice === "Scissors") {
+      result = "You lose. You are going to Hell."
       } else {
-        result = "You suck"
+      result = "Congratulations, you do not have to go to hell."
       }
       break;
-    case "Scissors":
-      if (userChoice === computerResult) {
-        result = "Draw Idiot"
-      } else if (userChoice === "paper") {
-        result = "You suck"
+      case "Scissors":
+      if (userChoice === satanResult) {
+        result = "Draw. Nice. One more chance to dance with the devil."
+      } else if (userChoice === "Paper") {
+        result = "You lose. You are going to Hell."
       } else {
-        result = "WINNER WINNER CHICKEN DINNER"
+        result = "Congratulations, you do not have to go to hell."
       }
       break;
-  }
-  displayResult()
+    }
+    displayResult()
+  };
+
+function displayResult() {
+  var resultContainer = document.body.querySelector("#result");
+  resultContainer.innerHTML = result;
+  var choiceContainer = document.body.querySelector("#choice");
+  choiceContainer.innerHTML = `Satan chose ${satanResult}`;
 };
